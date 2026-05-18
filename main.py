a5ccc64a487e4958102739e5c2718241251eb74e
DaiRen25 <kyithar212121@gmail.com>
2026-05-18 20:51:43 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from flask import Flask
-from threading import Thread
 import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-app_web = Flask(__name__)
-
-@app_web.route('/')
-def home():
-    return "Bot Online"
-
-def run_web():
-    app_web.run(host='0.0.0.0', port=10000)
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "✅ Bot Online!"
-    )
+    await update.message.reply_text("Bot Online ✅")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
-Thread(target=run_web).start()
+print("Bot Online")
 
 app.run_polling()
